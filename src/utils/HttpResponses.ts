aea97bfb68c031a57fd99124f958d43ed4099db3
Antonio <antonioimportant@gmail.com>
2024-04-17 15:30:04 -0300

    feat: changes http response structure

--- src/utils/HttpResponses.ts
+++ src/utils/HttpResponses.ts
@@ -1,28 +1,28 @@
-import { ResponseSchema } from "../interfaces/IHttp";
+import { IResponseSchema } from "../interfaces/IHttp";
 
 export class HttpResponse {
-  static _200(body = { message: "Success." }): ResponseSchema {
+  static _200(body: object = { message: "Success." }): IResponseSchema {
     return {
       statusCode: 200,
       body,
     }
   }
 
-  static _201(body = { message: "Created." }): ResponseSchema {
+  static _201(body: object = { message: "Created." }): IResponseSchema {
     return {
       statusCode: 201,
       body,
     }
   }
 
-  static _400(body = { message: "Bad Request." }): ResponseSchema {
+  static _400(body: object = { message: "Bad Request." }): IResponseSchema {
     return {
       statusCode: 400,
       body,
     }
   }
 
-  static _500(body = { message: "Internal Server Error." }): ResponseSchema {
+  static _500(body: object = { message: "Internal Server Error." }): IResponseSchema {
     return {
       statusCode: 500,
       body,
